a4ee242cf5fa80b0bd88232842b328a1e25a6336
Dr Trog <trogtrogalstaid@gmail.com>
2023-12-17 00:07:05 -0600

    Replacing all instances of Boilerplate with OutbreakUndead2e

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -2,7 +2,7 @@
  * Extend the basic Item with some very simple modifications.
  * @extends {Item}
  */
-export class BoilerplateItem extends Item {
+export class OutbreakUndead2eItem extends Item {
   /**
    * Augment the basic Item data model with additional dynamic data.
    */
--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -7,9 +7,9 @@
   return loadTemplates([
 
     // Actor partials.
-    "systems/boilerplate/templates/actor/parts/actor-features.html",
-    "systems/boilerplate/templates/actor/parts/actor-items.html",
-    "systems/boilerplate/templates/actor/parts/actor-spells.html",
-    "systems/boilerplate/templates/actor/parts/actor-effects.html",
+    "systems/outbreakundead2e/templates/actor/parts/actor-features.html",
+    "systems/outbreakundead2e/templates/actor/parts/actor-items.html",
+    "systems/outbreakundead2e/templates/actor/parts/actor-spells.html",
+    "systems/outbreakundead2e/templates/actor/parts/actor-effects.html",
   ]);
 };
